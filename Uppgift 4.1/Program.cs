c79c056bd9803773c8afb3a66924b3daa5d54bfe
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-03 21:55:23 +0200

    Add project files.

---
+++ Uppgift 4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i <= 30; i++)
+        {
+            Console.Write(i + " \t ");
+        }
+        Console.WriteLine();
+        for (int i = 200; i >= 180; i--)
+        {
+            Console.Write(i + " \t ");
+        }
+        Console.WriteLine();
+        for (int i = 1000; i <= 1400; i += 50)
+        {
+            Console.Write(i + " \t ");
+        }
+    }
+}
